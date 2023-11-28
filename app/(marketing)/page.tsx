93594c340de56b1dbb3bf07a94015f86c668238b
Ron Almog <ron8132@gmail.com>
2023-11-27 22:48:15 -0500

    add shadcn component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -13,6 +13,11 @@ const MarketingPage = () => {
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
           work forward.
         </div>
+        <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+          Collaborage, manage projects, and reach new productivity peaks. From
+          high rises to home office, the way your team works is unique.
+          Accomplish it all with Taskify
+        </div>
       </div>
     </div>
   );
