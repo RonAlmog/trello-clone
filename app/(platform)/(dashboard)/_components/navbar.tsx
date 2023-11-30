ce9024632ef35b33a41b0fdb723ef50abc6dd66d
Ron Almog <ron8132@gmail.com>
2023-11-29 20:42:49 -0500

    add nav bar to organization

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,19 @@
+import Logo from "@/components/logo";
 import React from "react";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      {/* mobile sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
